7d82e79f28f298bdad7dd31c70dd5738a7c4ed67
Joel Hadley <26hadlejoes@washk12.org>
2024-11-12 13:01:32 -0700

    Added arraystrings.py, unfinished

---
+++ arraysstrings.py
@@ -0,0 +1,30 @@
+#1
+sum_even_numbers[1,2,3,4,5,6]
+for item in sum_even_numbers:
+    if item % 2 == 0:
+        new_list == 0 + item
+    else:
+        item == 0
+print(new_list)
+
+
+#2
+
+#3
+filtererer = filter_negative_numbers(int([-1, 0, 2, -3, 5]))
+for item in filter_negative_numbers:
+    if item < 0:
+        item == 0
+    else:
+        item 
+
+
+#4
+
+#5
+reversing = reverse_list([1, 2, 3])
+reversing[2] = newreverse_list[0]
+reversing[1] = newreverse_list[1]
+reversing[0] = newreverse_list[2]
+print(newreverse_list)
+#6
