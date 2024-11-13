a5c243e1e2dd2bda06a1623536a65225dc45da61
Joel Hadley <26hadlejoes@washk12.org>
2024-11-12 17:16:53 -0700

    Update arraysstrings.py, still unfinished

--- arraysstrings.py
+++ arraysstrings.py
@@ -1,30 +1,31 @@
 #1
-sum_even_numbers[1,2,3,4,5,6]
-for item in sum_even_numbers:
-    if item % 2 == 0:
-        new_list == 0 + item
-    else:
-        item == 0
-print(new_list)
+totals = 0 #SET TOTAL TO 0 TO RESET
 
+def sum_even_numbers(numbers, totals): #INPUT TOTALS AND NUMBERS VARIALBES
+    for number in numbers:
+        if number % 2 == 0:
+            totals += number #IF NUMBER IS MODULUS 2 == 0, THE NUMBER IS ADDED TO THE TOTAL
+    return totals #SEND FINISHED TOTALS
+
+numbers = [1,2,3,4,5,6] #SET NUMBERS
+totals = sum_even_numbers(numbers, totals) #GET TOTAL AFTER THE FUNCTION RUNS           
+print(totals) #PRINT TOTALS
 
 #2
+message = "Hello"
+#SET MESSAGE
+def char_frequency(message): #DEFINE FUNCTION, ADD MESSAGE INTO PARAMETERS
+    letter_dict = {}  #QUICK DICTIONARY SETUP
+    for letter in message: #CHECK THE LETTER
+        if letter in letter_dict: #THIS CODE JUST SAYS BASICALLY: IF LETTER ISNT ALREADY IN THE DICTIONARY ADD IT
+            letter_dict[letter] += 1
+        else:
+            letter_dict[letter] = 1
+    return letter_dict #RETURN DICTIONARY COMPLETED
+
+lettercount = char_frequency(message) #FUNCTION RUNS, RESULT GIVEN
+print(lettercount)
+
 
 #3
-filtererer = filter_negative_numbers(int([-1, 0, 2, -3, 5]))
-for item in filter_negative_numbers:
-    if item < 0:
-        item == 0
-    else:
-        item 
 
-
-#4
-
-#5
-reversing = reverse_list([1, 2, 3])
-reversing[2] = newreverse_list[0]
-reversing[1] = newreverse_list[1]
-reversing[0] = newreverse_list[2]
-print(newreverse_list)
-#6
